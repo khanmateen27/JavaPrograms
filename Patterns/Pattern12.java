6340668a0d490513901f45ba25634a5cfd10101b
khanmateen27 <khanmateen2712@gmail.com>
2023-02-09 21:31:54 +0530

    first commit

---
+++ Patterns/Pattern12.java
@@ -0,0 +1,16 @@
+package Patterns;
+public class Pattern12
+{            
+    public static void main(String[] args)
+    {
+        int alphabet = 65;
+                for (int i = 0; i <= 5; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                System.out.print((char) (alphabet + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
