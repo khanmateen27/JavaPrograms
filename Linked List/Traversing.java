59e774fd5f4c6d3cccccd1ea982d6297165eaf84
Mohd Mateen Khan <67383719+khanmateen27@users.noreply.github.com>
2023-02-11 23:55:07 +0530

    Add files via upload

---
+++ Linked List/Traversing.java
@@ -0,0 +1,25 @@
+class Node{
+    int data;
+    Node next;
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+
+class Test{
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next  = new Node(30);
+        head.next.next.next = new Node(40);
+        printLinkedList(head);
+    }
+    public static void printLinkedList(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+}
